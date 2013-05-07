cfbe73f1f131c0a74ab1ee48e935a345b9df2ba4
Patrick Bartsch <bartsch98@gmail.com>
2013-05-07 22:49:07 +0200

    added demo

---
+++ demo/app/scripts/controllers/main.js
@@ -0,0 +1,47 @@
+'use strict';
+
+angular.module('demoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'scroll me down',
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma',
+      'iScroll',
+      'momentum',
+      'Demo',
+      'Github',
+      'HTML5',
+      'Javascript',
+      '-- and again --',
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma',
+      'iScroll',
+      'momentum',
+      'Demo',
+      'Github',
+      'HTML5',
+      'Javascript',
+      '-- and again --',
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma',
+      'iScroll',
+      'momentum',
+      'Demo',
+      'Github',
+      'HTML5',
+      'Javascript',
+      '-- and again --',
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma',
+      'iScroll',
+      'momentum',
+      'Demo',
+      'Github',
+      'HTML5',
+      'Javascript'
+    ];
+  });
