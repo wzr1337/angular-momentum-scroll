6801f402deb25baa699804ab5d766c5cbd54ea5d
Patrick Bartsch <bartsch98@gmail.com>
2013-05-07 22:04:16 +0200

    v0.2.0 fix(jshint) changed linting rules feat(scrollToPage) one can now programatically change pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,68 +1,66 @@
 'use strict';
 
 module.exports = function(grunt) {
-    // load all grunt tasks
-    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+  // load all grunt tasks
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-    grunt.initConfig({
-        concat : {
-            dist : {
-                files : {
-                    'dist/scrollable.js' : [
-                        'components/iscroll/src/iscroll.js',
-                        'src/scrollable.js'
-                    ]
-                }
-            }
-        },
-        clean : {
-            dist : {
-                files : [ {
-                    dot : true,
-                    src : [ 'dist/**' ]
-                } ]
-            },
-            server : '.tmp'
-        },
-        uglify: {
-            dist: {
-                files: { 'dist/scrollable.min.js': [ 'dist/scrollable.js' ] }
-            }
-        },
-        jshint : {
-            options : {
-                jshintrc : '.jshintrc'
-            },
-            all : [ 'Gruntfile.js', 'src/*.js' ]
-        },
-        karma : {
-            unit : {
-                configFile : 'karma.conf.js',
-                singleRun : true
-            }
-        },
-        compress: {
-            main: {
-                options: {
-                    archive: 'angular-momentum-scroll.zip'
-                },
-                files: [{flatten: true,
-                    src: ['dist/*'],
-                    dest: 'angular-momentum-scroll/',
-                    filter: 'isFile'}]
-            }
+  grunt.initConfig({
+    concat : {
+      dist : {
+        files : {
+          'dist/scrollable.js' : [
+            'components/iscroll/src/iscroll.js',
+            'src/scrollable.js'
+          ]
         }
-    });
+      }
+    },
+    clean : {
+      dist : {
+        files : [ {
+          dot : true,
+          src : [ 'dist/**' ]
+        } ]
+      },
+      server : '.tmp'
+    },
+    uglify: {
+      dist: {
+        files: { 'dist/scrollable.min.js': [ 'dist/scrollable.js' ] }
+      }
+    },
+    jshint : {
+      options : {
+        jshintrc : '.jshintrc'
+      },
+      all : [ 'Gruntfile.js', 'src/*.js' ]
+    },
+    karma : {
+      unit : {
+        configFile : 'karma.conf.js',
+        singleRun : true
+      }
+    },
+    compress: {
+      main: {
+        options: {
+          archive: 'angular-momentum-scroll.zip'
+        },
+        files: [{flatten: true,
+          src: ['dist/*'],
+          dest: 'angular-momentum-scroll/',
+          filter: 'isFile'}]
+      }
+    }
+  });
 
-    grunt.registerTask('build', [
-        'clean:dist',
-        'jshint',
-        'concat',
-        'uglify',
-        'compress'
-    ]);
+  grunt.registerTask('build', [ 'clean:dist',
+                                'jshint',
+                                'concat',
+                                'uglify',
+                                'compress' ]);
 
-    grunt.registerTask('test', ['karma']);
+  grunt.registerTask('test', ['karma']);
 
-    grunt.registerTask('default', [ 'build' ]);
+  grunt.registerTask('default', [ 'build' ]);
 };
