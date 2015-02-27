2e41e6ce8b66e881fa0ad616154ad81df5bdc36a
SaggioFilosofo <saggiofilosofo@gmail.com>
2015-02-27 16:53:00 +0100

    Fix the iScrollInstance duplication
    The scrollbar (iScrollInstance) was doubled when invoked the refresh.
    
    Moving iScrollInstance.destroy(); within the "timeout", the bug is fixed.

--- src/scrollable.js
+++ src/scrollable.js
@@ -227,10 +227,10 @@ angular.module('angular-momentum-scroll').directive('scrollable',
 
         var _refresh = function(nVal) {
             if (angular.isDefined(nVal)) {
-              if (angular.isDefined(iScrollInstance)) {
-                iScrollInstance.destroy();
-              }
               $timeout(function(){
+                if (angular.isDefined(iScrollInstance)) {
+                  iScrollInstance.destroy();
+                }
                 _init();
                 if (angular.isDefined(iScrollInstance.pages) &&
                   iScrollInstance.pages.length > 0) {
